1e12b47b14a8b6ae3594b4e069cb791726d220e5
Vetala13 <89152440+Vetala13@users.noreply.github.com>
2022-03-25 21:49:22 +0300

    Add files via upload
    Добавлен фал с выполненным домашним задание по второму уроку.

---
+++ JavaHomeWork2.java
@@ -0,0 +1,33 @@
+/**
+ * Java 1. Homework 2
+ *
+ * @autor Anna M.
+ * @version 25.03.2022
+ */
+class JavaHomeWork2 {
+
+    public static void main(String[] args) {
+        System.out.println(checkRangeSum(13, 13));
+        printPositiveOrNegative(13);
+        System.out.println(numberIsNegative(-13));
+        printString();
+}
+
+    static boolean checkRangeSum(int a, int b) {
+        return 10 <= a + b && a + b <= 20;
+    }
+    
+    static void printPositiveOrNegative(int c) {
+        System.out.println(c >= 0? "Positive" : "Negative");
+    }
+    
+    static boolean numberIsNegative(int d) {
+        return (d < 0);
+    }
+    
+    static void printString() {
+        int i;
+        for (i = 1; i <= 5; i++) 
+            System.out.println("I love Java");
+        }
+    }
